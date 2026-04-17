134cce8982ca20adba199311ad6812003bb52d9a
Barca6ta2024 <migueldegollado20@gmail.com>
2026-04-17 14:51:59 -0700

    Add Share route to NavRoutes

--- app/src/main/java/com/oracle/visualize/domain/models/NavRoutes.kt
+++ app/src/main/java/com/oracle/visualize/domain/models/NavRoutes.kt
@@ -3,11 +3,8 @@ package com.oracle.visualize.domain.models
 sealed class NavRoutes(val route: String) {
     object Feed : NavRoutes("feed")
     object Notifications : NavRoutes("notifications")
-
     object Profile : NavRoutes("profile")
-
     object Teams : NavRoutes("teams")
-
     object Create : NavRoutes("create")
-
+    object Share : NavRoutes("share")
 }
