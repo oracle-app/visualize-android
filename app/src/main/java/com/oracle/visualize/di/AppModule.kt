50ef82281c72e54c5ebe70b062de0e56cb5d849c
Crlss123 <camadortjr@gmail.com>
2026-04-19 22:36:37 -0700

    fix: Changed Singleton to SingletonComponent in AppModule. Now it is working fine.

--- app/src/main/java/com/oracle/visualize/di/AppModule.kt
+++ app/src/main/java/com/oracle/visualize/di/AppModule.kt
@@ -3,10 +3,11 @@ package com.oracle.visualize.di
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(Singleton::class)
+@InstallIn(SingletonComponent::class)
 object AppModule {
 
 //    Example
