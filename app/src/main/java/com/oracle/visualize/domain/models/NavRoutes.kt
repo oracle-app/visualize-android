e65928d57ec6c9934d443fdb898eb0326cb30169
Barca6ta2024 <migueldegollado20@gmail.com>
2026-04-19 19:48:07 -0700

    Refactor BottomNavBar to use NavController instead of selectedIndex

--- app/src/main/java/com/oracle/visualize/domain/models/NavRoutes.kt
+++ app/src/main/java/com/oracle/visualize/domain/models/NavRoutes.kt
@@ -1,13 +1,10 @@
 package com.oracle.visualize.domain.models
 
 sealed class NavRoutes(val route: String) {
-    object Feed : NavRoutes("feed")
+    object Feed          : NavRoutes("feed")
     object Notifications : NavRoutes("notifications")
-
-    object Profile : NavRoutes("profile")
-
-    object Teams : NavRoutes("teams")
-
-    object Create : NavRoutes("create")
-
+    object Profile       : NavRoutes("profile")
+    object Teams         : NavRoutes("teams")
+    object Create        : NavRoutes("create")
+    //object Share         : NavRoutes("share")
 }
