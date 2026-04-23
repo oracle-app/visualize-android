31cfd448d4ad3e676f8e96420dcb90d15469ccf5
LuisP Cardenas <a01254955@tec.mx>
2026-04-23 04:36:04 -0700

    Merge: change_viz_management_to_jetpack_navigation with develop

--- app/src/main/java/com/oracle/visualize/presentation/screens/login/LoginScreen.kt
+++ app/src/main/java/com/oracle/visualize/presentation/screens/login/LoginScreen.kt
@@ -14,18 +14,4 @@ fun LoginScreen(
     viewModel: LoginViewModel = hiltViewModel()
     ) {
 
-    // ScreenBackground 0xFFF5F4F2 -> Background
-    // TealPrimary = 0xFF34797C -> Login button
-    // TextDark = 0xFF13212C -> Welcome Text
-    // TextGray = 0xFF323232 ->  show password & hide password
-    // Green = 0xFFE6EDEC -> Input Text Field
-    // TextGrayLight = 0xFF597271 -> Don't have an account?
-    // TextOrange = 0xFFE18212 -> Sign Up
-    // TextVersion = 0xFF798B8A -> Version
-    // ErrorAuthInput = 0xFFFFDAD6 -> Error Text Field
-    // ErrorAuthText = 0xFFBA1A1A  -> Error Text (Inputs)
-    // TextHint = 0xFF49454F -> Hint Text
-
-
-
 }
