2c8824e08b976c8db3f68519530c6947562ded28
Crlss123 <camadortjr@gmail.com>
2026-04-26 15:58:13 -0700

    fix: Removes @DocumentID from visualization model and changes Timestamp (firebase typing) to native Date

--- app/src/main/java/com/oracle/visualize/domain/models/Visualization.kt
+++ app/src/main/java/com/oracle/visualize/domain/models/Visualization.kt
@@ -1,15 +1,13 @@
 package com.oracle.visualize.domain.models
 
-import com.google.firebase.Timestamp
-import com.google.firebase.firestore.DocumentId
+import java.sql.Date
 
 data class Visualization (
-    @DocumentId
     val id: String,
     val authorID: String,
     val title: String,
     val configJSON: String,
     val sharedWithUsers: List<String>,
     val sharedWithTeams: List<String>,
-    val createdAt: Timestamp,
+    val createdAt: Date,
 )
