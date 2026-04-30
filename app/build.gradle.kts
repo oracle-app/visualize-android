d7e79dc9629c82257bfa34c171c49a7b70b4b78f
Barca6ta2024 <migueldegollado20@gmail.com>
2026-04-29 17:47:16 -0700

    Update project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,6 +45,7 @@ android {
 
 dependencies {
     implementation(libs.androidx.core.ktx)
+    implementation("com.google.dagger:hilt-android:2.51.1")
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
