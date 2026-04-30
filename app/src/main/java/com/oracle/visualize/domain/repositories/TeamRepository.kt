51b305b83f31e3392d2e4f191c8f2315d2e72dbc
Barca6ta2024 <migueldegollado20@gmail.com>
2026-04-29 17:23:01 -0700

    Add updateTeam and deleteTeam to TeamRepository interface

--- app/src/main/java/com/oracle/visualize/domain/repositories/TeamRepository.kt
+++ app/src/main/java/com/oracle/visualize/domain/repositories/TeamRepository.kt
@@ -5,7 +5,12 @@ import com.oracle.visualize.domain.models.Team
 interface TeamRepository {
     suspend fun createTeam(memberIDs: List<String>, name: String, ownerID: String)
 
+    suspend fun updateTeam(teamID: String, memberIDs: List<String>, name: String)
+
     suspend fun getTeamsOwnedByUser(userID: String): List<ShareTeam>
+
+    suspend fun deleteTeam(teamID: String)
+
 
     suspend fun getTeamsUserIsIn(userID: String): List<ShareTeam>
 
