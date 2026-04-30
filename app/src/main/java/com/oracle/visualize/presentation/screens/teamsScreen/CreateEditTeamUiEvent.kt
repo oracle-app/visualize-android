69bcb6df1876145a1af57523f42203023e8400ec
Barca6ta2024 <migueldegollado20@gmail.com>
2026-04-29 17:44:12 -0700

    Add CreateEditTeamUiEvent sealed interface for create and edit form events

---
+++ app/src/main/java/com/oracle/visualize/presentation/screens/teamsScreen/CreateEditTeamUiEvent.kt
@@ -0,0 +1,11 @@
+package com.oracle.visualize.presentation.screens.teamsScreen
+
+import com.oracle.visualize.domain.models.ShareUser
+
+sealed interface CreateEditTeamUiEvent {
+    data class NameChanged(val name: String) : CreateEditTeamUiEvent
+    data class SearchQueryChanged(val query: String) : CreateEditTeamUiEvent
+    data class AddMember(val user: ShareUser) : CreateEditTeamUiEvent
+    data class RemoveMember(val user: ShareUser) : CreateEditTeamUiEvent
+    object Submit : CreateEditTeamUiEvent
+}
