507620e41eec7d3840bc57417582d27439eaa3ac
eduardocardenasv27 <a00232432@tec.mx>
2026-05-04 20:04:37 -0600

    Implemented setProfilePicture to  UserRepository.kt

--- app/src/main/java/com/oracle/visualize/domain/repositories/UserRepository.kt
+++ app/src/main/java/com/oracle/visualize/domain/repositories/UserRepository.kt
@@ -3,6 +3,7 @@ package com.oracle.visualize.domain.repositories
 import com.oracle.visualize.domain.models.ShareUser
 import com.oracle.visualize.domain.models.Team
 import com.oracle.visualize.domain.models.User
+import android.net.Uri
 
 /**
  * Interface defining the operations for user management.
@@ -11,5 +12,7 @@ interface UserRepository {
     suspend fun getUserByUserID(userId: String): User?
     suspend fun getTeamsIntegratedByUser(userId: String): List<Team>
     suspend fun getUserSuggestionsByEmail(email: String): List<ShareUser>
+
+    suspend fun setProfilePicture(userID: String, uri: Uri): Boolean
 
 }
