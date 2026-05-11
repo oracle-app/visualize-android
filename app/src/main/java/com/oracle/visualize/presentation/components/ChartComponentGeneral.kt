a52ea87cb3c686a7f15d8f582eaad593c64f73b9
UzielMedina22 <uziel.jonathan22@gmail.com>
2026-05-10 20:51:51 -0700

    Clean imports in ChartComponentGeneral.kt

--- app/src/main/java/com/oracle/visualize/presentation/components/ChartComponentGeneral.kt
+++ app/src/main/java/com/oracle/visualize/presentation/components/ChartComponentGeneral.kt
@@ -3,7 +3,6 @@ package com.oracle.visualize.presentation.components
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.MaterialTheme
