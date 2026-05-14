1cbf68aca5ca81d2b49d370f5a4f053133d6bb70
Barca6ta2024 <migueldegollado20@gmail.com>
2026-05-13 23:52:04 -0700

    document AddMember/RemoveMember as edit-mode-only events

--- app/src/main/java/com/oracle/visualize/presentation/screens/createEditScreen/CreateEditTeamUiEvent.kt
+++ app/src/main/java/com/oracle/visualize/presentation/screens/createEditScreen/CreateEditTeamUiEvent.kt
@@ -5,7 +5,12 @@ import com.oracle.visualize.domain.models.ShareUser
 sealed interface CreateEditTeamUiEvent {
     data class NameChanged(val name: String) : CreateEditTeamUiEvent
     data class SearchQueryChanged(val query: String) : CreateEditTeamUiEvent
+
+
     data class AddMember(val user: ShareUser) : CreateEditTeamUiEvent
+
+    /** @see AddMember */
     data class RemoveMember(val user: ShareUser) : CreateEditTeamUiEvent
+
     object Submit : CreateEditTeamUiEvent
 }
