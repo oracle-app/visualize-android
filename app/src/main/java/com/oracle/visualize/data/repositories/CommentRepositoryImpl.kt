8bd8daa077bd969b7914f76e0dee248c70fde720
sebas <A00227588@tec.mx>
2026-05-25 20:56:26 -0700

    Fix comment loading times

--- app/src/main/java/com/oracle/visualize/data/repositories/CommentRepositoryImpl.kt
+++ app/src/main/java/com/oracle/visualize/data/repositories/CommentRepositoryImpl.kt
@@ -18,15 +18,21 @@ class CommentRepositoryImpl @Inject constructor(
         authorID: String,
         content: String,
         imageURL: String?
-    ) {
-        commentDatasource.createComment(
-            visualizationId = visualizationId,
-            commentDTO = CommentDTO(
-                authorID = authorID,
-                content = content,
-                imageURL = imageURL
-            )
+    ): Comment {
+        val commentDTO = CommentDTO(
+            authorID = authorID,
+            content = content,
+            imageURL = imageURL
         )
+
+        val id = commentDatasource.createComment(
+            visualizationId = visualizationId,
+            commentDTO = commentDTO
+        )
+
+        return commentDTO
+            .copy(id = id)
+            .toDomain()
     }
 
     override suspend fun getComments(
@@ -56,16 +62,22 @@ class CommentRepositoryImpl @Inject constructor(
         authorName: String,
         authorAvatarURL: String?,
         content: String
-    ) {
-        commentDatasource.createThread(
+    ): Thread {
+        val threadDTO = ThreadDTO(
+            authorID = authorID,
+            authorName = authorName,
+            authorAvatarURL = authorAvatarURL,
+            content = content
+        )
+
+        val id = commentDatasource.createThread(
             visualizationId = visualizationId,
             commentId = commentId,
-            threadDTO = ThreadDTO(
-                authorID = authorID,
-                authorName = authorName,
-                authorAvatarURL = authorAvatarURL,
-                content = content
-            )
+            threadDTO = threadDTO
         )
+
+        return threadDTO
+            .copy(id = id)
+            .toDomain()
     }
 }
