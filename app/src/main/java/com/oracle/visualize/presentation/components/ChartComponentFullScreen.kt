49b813bdfe270427b3de66348c2ebc453a5267e7
UzielMedina22 <uziel.jonathan22@gmail.com>
2026-05-26 12:27:12 -0700

    Fix. Modified legend layout distribution, depending on orientation.

--- app/src/main/java/com/oracle/visualize/presentation/components/ChartComponentFullScreen.kt
+++ app/src/main/java/com/oracle/visualize/presentation/components/ChartComponentFullScreen.kt
@@ -1,21 +1,27 @@
 package com.oracle.visualize.presentation.components
 
+import android.content.res.Configuration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.oracle.visualize.R
@@ -26,8 +32,9 @@ import com.oracle.visualize.domain.models.ScatterChart
 import com.oracle.visualize.domain.models.VerticalBarChart
 import io.github.koalaplot.core.ChartLayout
 import io.github.koalaplot.core.Symbol
-import io.github.koalaplot.core.legend.FlowLegend
+import io.github.koalaplot.core.legend.FlowLegend2
 import io.github.koalaplot.core.legend.LegendLocation
+import io.github.koalaplot.core.style.KoalaPlotTheme
 import io.github.koalaplot.core.util.ExperimentalKoalaPlotApi
 
 /**
@@ -45,19 +52,25 @@ fun ChartRenderFullScreen(
     chart: Chart<*>,
     showAxisLabels: Boolean = true
 ) {
-    val labels = chart.fieldNames
+    val localConfig = LocalConfiguration.current
 
-    val dSize = when (chart.data) {
-        is List<*> -> chart.data.size
-        is Map<*,*> -> (chart.data.values.firstOrNull() as? List<*>)?.size ?: 1
-        else -> 1
+    val labels = remember(chart)  { chart.fieldNames }
+
+    val dSize = remember(chart.data) {
+        when (chart.data) {
+            is List<*> -> chart.data.size
+            is Map<*,*> -> (chart.data.values.firstOrNull() as? List<*>)?.size ?: 1
+            else -> 1
+        }
     }
 
     val cleanLabels = labels.ifEmpty {
         List(dSize) { "${stringResource(R.string.chart_legend_cat_label)} ${it + 1}" }
     }
 
-    val colors = generateChartColors(cleanLabels.size)
+    val colors = remember(cleanLabels.size) {
+        generateChartColors(cleanLabels.size)
+    }
 
     when (chart) {
         is VerticalBarChart, is HorizontalBarChart, is LineChart, is ScatterChart -> {
@@ -68,41 +81,54 @@ fun ChartRenderFullScreen(
                 }
             }
         }
+
         else -> {
+            val flowLegendContent = @Composable {
+                FlowLegend2(
+                    itemCount = cleanLabels.size,
+                    modifier = Modifier.wrapContentSize().padding(14.dp),
+                    symbol = { Symbol(shape = CircleShape, fillBrush = SolidColor(colors[it])) },
+                    label = { Text(cleanLabels[it], style = MaterialTheme.typography.bodyLarge, color = Color.DarkGray) },
+                    symbolGap = KoalaPlotTheme.sizes.gap,
+                    columnGap = KoalaPlotTheme.sizes.gap,
+                    rowGap = KoalaPlotTheme.sizes.gap
+                )
+            }
+
             ChartLayout(
                 modifier = Modifier.fillMaxSize(),
                 title = {},
                 legend = {
-                    Box(modifier = Modifier.fillMaxWidth().padding(vertical = 16.dp, horizontal = 2.dp)) {
-                        FlowLegend(
-                            modifier = Modifier
-                                .border(1.dp,
-                                    MaterialTheme.colorScheme.onPrimaryContainer,
-                                    RoundedCornerShape(12.dp)
-                                )
-                                .background(
-                                    MaterialTheme.colorScheme.onPrimary,
-                                    RoundedCornerShape(12.dp)
-                                )
-                                .padding(16.dp).align(Alignment.Center),
-                            itemCount = cleanLabels.size,
-                            symbol = { Symbol(
-                                shape = CircleShape,
-                                fillBrush = SolidColor(colors[it])
-                            ) },
-                            label = {
-                                Text(
-                                    cleanLabels[it],
-                                    style = MaterialTheme.typography.bodyLarge,
-                                    color = Color.DarkGray
-                                ) }
-                        )
+                    when (localConfig.orientation) {
+                        Configuration.ORIENTATION_LANDSCAPE -> {
+                            Column(
+                                modifier = Modifier.fillMaxHeight()
+                                    .background(color = MaterialTheme.colorScheme.onPrimary),
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                flowLegendContent()
+                            }
+                        }
+                        else -> {
+                            Row(
+                                modifier = Modifier.fillMaxWidth()
+                                    .background(color = MaterialTheme.colorScheme.onPrimary),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.Center
+                            ) {
+                                flowLegendContent()
+                            }
+                        }
                     }
                 },
-                legendLocation = LegendLocation.TOP
+                legendLocation = when (localConfig.orientation) {
+                    Configuration.ORIENTATION_LANDSCAPE -> LegendLocation.LEFT
+                    else -> LegendLocation.TOP
+                }
             ) {
-                Column(modifier = modifier.fillMaxSize()) {
-                    Column(modifier = modifier.background(color = MaterialTheme.colorScheme.onPrimary)
+                Box(modifier = modifier.fillMaxSize()) {
+                    Box(modifier = modifier.background(color = MaterialTheme.colorScheme.onPrimary)
                         .padding(horizontal = 10.dp).fillMaxSize()) {
                         ChartRenderGeneral(modifier, chart, showAxisLabels)
                     }
