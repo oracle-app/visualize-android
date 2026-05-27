da23a6489fc532aadc5f8c75e37b73f932daf5f5
Miguel Degollado <A01255388@tec.mx>
2026-05-27 14:35:25 -0700

    feat(UserRepository): add getUsersByIDs to resolve user IDs to ShareUser objects

--- app/src/main/java/com/oracle/visualize/domain/repositories/UserRepository.kt
+++ app/src/main/java/com/oracle/visualize/domain/repositories/UserRepository.kt
@@ -3,10 +3,8 @@ package com.oracle.visualize.domain.repositories
 import com.oracle.visualize.domain.models.ShareUser
 import com.oracle.visualize.domain.models.User
 
-/**
- * Interface defining the operations for user management.
- */
 interface UserRepository {
+    suspend fun getUsersByIDs(userIDs: List<String>): List<ShareUser>
     suspend fun getUserSuggestionsByEmail(email: String): List<ShareUser>
     suspend fun getUserByUserID(userID: String): User?
 }
